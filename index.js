0601fab3425b002e267c78e45daa653372691609
shivisweartogod <shivisweartogod@gmail.com>
2023-09-10 19:01:19 +0530

    added typing text in profile

--- index.js
+++ index.js
@@ -4,4 +4,20 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  const text= document.querySelector(".text-2");
+  const textLoad=() =>{
+    setTimeout(()=>{
+      text.textContent="Software Developer"
+    }, 0);
+    setTimeout(()=>{
+      text.textContent="Data Scientist"
+    }, 4000);
+    setTimeout(()=>{
+      text.textContent="Web Developer"
+    }, 8000);
+  }
+
+  textLoad();
+  setInterval(textLoad,12000);
   
