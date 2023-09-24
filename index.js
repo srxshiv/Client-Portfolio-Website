9812275c1dd5fa8caa37f07857d2991fb2cb985f
shivisweartogod <shivisweartogod@gmail.com>
2023-09-24 19:46:00 +0530

    contact form working, #COMPLETED

--- index.js
+++ index.js
@@ -57,27 +57,6 @@ function toggleMenu() {
   textLoad();
   setInterval(textLoad,12000);
 
-  // contact form ka
-  function sendMail(){
-    Email.send({
-      // SecureToken: "c93a4673-7dcd-4b76-8790-05f1a3fe51e4",
-      Host: "smtp.elasticemail.com",
-      Username: "divyanshucs127@gmail.com",
-      Password: "BF9E3E1787446468AAA4EE09E5652058AC00",
-      To : "divyanshucs127@gmail.com",
-      From : document.querySelector("#contact-email").value,
-      Subject : "New Enquiry from Website",
-      Body : "Name : " + document.querySelector("#contact-name").value
-           + "<br> Email : "+ document.querySelector("#contact-email").value
-           + "<br> Phone no. : "+ document.querySelector("#contact-phone").value
-           + "<br> Message: " +document.querySelector("#contact-msg").value
-  }).then(
-    message=> alert("Message Sent Succesfully"),
-    console.log("work")
-  )
-  }
-
-
   // SCROLL ANIMATIONSS
 
 
